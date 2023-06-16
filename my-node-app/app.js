c9c6bd2a9cd486417cae8fd1f1d8515f3356e142
Manuel Tec <72881652+Manuel17062001@users.noreply.github.com>
2023-06-15 18:23:04 -0600

    app

--- my-node-app/app.js
+++ my-node-app/app.js
@@ -3,11 +3,15 @@ const app = express();
 const port = 3000;
 
 // Define a route for the root URL
+const path = require('path');
+
+// Define a route for the root URL
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
